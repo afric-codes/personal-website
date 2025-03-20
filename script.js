356a791edda78defb7610762c5f143b13e21e563
afric <africdonegal@gmail.com>
2025-03-20 21:03:09 +0000

    implement changed colour on hover, double-click to open, and custom font

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function switchToDark() {
     document.getElementById("theme").href = "night.css";
-    document.getElementById("nightday").onclick = switchToLight;
+    document.getElementById("nightday").ondblclick = switchToLight;
 }
 
 function switchToLight() {
     // Switch the stylesheet to the day ver
     document.getElementById("theme").href = "day.css";
     // Switch the function to the darkmode function so we can get back to dark mode
-    document.getElementById("nightday").onclick = switchToDark;
+    document.getElementById("nightday").ondblclick = switchToDark;
 }
