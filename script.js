962e0e4021946b86fbf31632b7c4d230a7ca0e5c
afric <afriic@proton.me>
2025-03-20 19:03:46 +0000

    make day/night switch functional and begin work on desktop interface

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function switchToDark() {
     document.getElementById("theme").href = "night.css";
-    alert("it works!");
+    document.getElementById("nightday").onclick = switchToLight;
+}
+
+function switchToLight() {
+    // Switch the stylesheet to the day ver
+    document.getElementById("theme").href = "day.css";
+    // Switch the function to the darkmode function so we can get back to dark mode
+    document.getElementById("nightday").onclick = switchToDark;
 }
