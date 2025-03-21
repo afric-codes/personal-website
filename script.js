ec42cc86d1e2ebee9d4146ac7d638cc5a8bd4bc5
afric <afriic@proton.me>
2025-03-21 17:02:52 +0000

    create about me button. not yet functional, css only on night mode

--- script.js
+++ script.js
@@ -11,3 +11,7 @@ function switchToLight() {
     // Switch the function to the darkmode function so we can get back to dark mode
     document.getElementById("nightday").ondblclick = switchToDark;
 }
+
+function aboutMe(){
+    alert("about me")
+}
